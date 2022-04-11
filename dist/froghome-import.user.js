c7317c372fdb9c47b483d2d1558f9a6fb6686b70
Kenny Chou <kenny@kramxel.com>
2022-04-11 15:00:47 +0800

    release 1.0.1

--- dist/froghome-import.user.js
+++ dist/froghome-import.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         TamperMonkey-Froghome
+// @name         froghome-import
 // @namespace    https://kennychou.github.io/2021/12/14/%E5%85%A9%E6%A3%B2%E5%BF%97%E5%B7%A5%E8%9B%99%E9%A1%9E%E8%AA%BF%E8%A8%98%E9%8C%84%E5%99%A8/
-// @version      1.0.0
+// @version      1.0.1
 // @description  使用Vue2.x建構TamperMonkey協助輸入兩棲調查資料
 // @author       Kenny Chou
 // @grant        GM_getValue
